204c351ca97124203e8f479a5f62515f8dac448c
1point21 <j.drinkall@mailfence.com>
2024-03-13 14:58:09 +0000

    feat: add functions for scan and total

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 class Checkout {
-    constructor(rules) {
-        this.rules = rules
-    }
+  constructor(rules) {
+    this.rules = rules;
+    this.items = {};
+  }
+  scan(item) {
+    if (this.items.hasOwnProperty(item)) this.items.item++;
+    else this.items.item = 1;
+  }
+  total() {
+    // need logic for totalling of items according to rules
+  }
 }
+
+module.exports = { Checkout };
