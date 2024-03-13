001c1843a82b626c055b2b83e822a163c73d2206
1point21 <j.drinkall@mailfence.com>
2024-03-13 16:40:46 +0000

    fix: correct syntax for total function

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 class Checkout {
   constructor(rules) {
     this.rules = rules;
-    this.items = {};
+    this._items = {};
   }
 
   scan(item) {
-    if (this.items.hasOwnProperty(item)) this.items[item]++;
-    else this.items[item] = 1;
+    if (this.rules.hasOwnProperty(item)) {
+      if (this._items.hasOwnProperty(item)) this._items[item]++;
+      else this._items[item] = 1;
+    }
   }
 
-  scanMultiple(multipleItems){
-    const itemArr = multipleItems.split('')
-    itemArr.forEach(item => {
-        this.scan(item)
-    })
+  scanMultiple(multipleItems) {
+    const itemArr = multipleItems.split("");
+    itemArr.forEach((item) => {
+      this.scan(item);
+    });
   }
-  
+
   total() {
-    let totalPrice = 0
-    for (let item in this.items){
-        if (this.rules[item][1]){
-            const numberSpecials = Math.floor(this.items[item] / this.rules[item][1].number)
-            const remainder = this.items[item] % this.rules[item][1].number
-            totalPrice += (numberSpecials * this.rules[item][1].cost) + (remainder * this.rules[item][0])
-        }
-        else{
-            totalPrice += this.items[item] * this.rules[item][0]
-        }
+    let totalPrice = 0;
+    for (let item in this._items) {
+      if (this.rules[item][1]) {
+        const numberSpecials = Math.floor(
+          this._items[item] / this.rules[item][1].number
+        );
+        const remainder = this._items[item] % this.rules[item][1].number;
+        totalPrice +=
+          numberSpecials * this.rules[item][1].cost +
+          remainder * this.rules[item][0];
+      } else {
+        totalPrice += this._items[item] * this.rules[item][0];
+      }
     }
-    return totalPrice
+    return totalPrice;
   }
 }
 
